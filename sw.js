7d81f2b0ff6fa70c249977fd2e07331f3e4fd748
BORI Deploy <bori-deploy@local>
2026-08-05 07:48:12 +0000

    Use real illustration images for create/join room cards

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "bori-v1.3.1-production";
-const ASSETS = ["./","./index.html","./styles.css","./config.js","./app.js","./manifest.webmanifest","./assets/favicon.png","./assets/icon-192.png","./assets/icon-512.png","./assets/splash-v2.png","./assets/hero-bori.jpg"];
+const ASSETS = ["./","./index.html","./styles.css","./config.js","./app.js","./manifest.webmanifest","./assets/favicon.png","./assets/icon-192.png","./assets/icon-512.png","./assets/splash-v2.png","./assets/hero-bori.jpg","./assets/action-door.jpg","./assets/action-key.jpg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))));
 self.addEventListener("fetch", event => {
