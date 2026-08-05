a2715d0653db4a79bb6679b48d1038c7ce057110
katy0409 <b900543@gmail.com>
2026-08-05 12:11:21 +0800

    commit

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = "bori-v1.2-cache-1";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./assets/icon-192.png","./assets/icon-512.png","./assets/splash-v2.png"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",event=>event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))));
