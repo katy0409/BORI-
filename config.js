b84ec2354ff9ee5c25f45bddec309faa9b305cf1
katy0409 <b900543@gmail.com>
2026-08-21 18:46:28 +0800

    commit

--- config.js
+++
@@ -1,6 +0,0 @@
-// BORI V1.4.0 正式版雲端設定
-// 僅專案擁有者需要設定一次；一般使用者不會看到此檔案或輸入畫面。
-window.BORI_CONFIG = {
-  SUPABASE_URL: "https://zvbcrpthpfyxteivfipj.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_1Wv-pjEkaPn242xxYIri2g_XmttdghB"
-};
